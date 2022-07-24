37ee0356fd227e4e10a01f1241e0400a1fbc434b
Kim Ngan Dinh Phan <kimngandinhphan@gmail.com>
2022-07-24 22:36:34 +0700

    init

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,18 @@
+﻿using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto() { Id = 1, Name = "New York City", Description = "The one is the most populous city in the United States."},
+                new CityDto() { Id = 2, Name = "Ho Chi Minh City", Description= "The one also known by its former name Saigon, is the largest city in Vietnam. "}
+            };
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
+        }
+    }
+}
