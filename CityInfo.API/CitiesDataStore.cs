2d9bff4cfad07e1b806162b697840c7dd9b9d0f5
Kim Ngan Dinh Phan <kimngandinhphan@gmail.com>
2022-08-03 23:01:38 +0700

    add content negotiation and return file

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -10,9 +10,51 @@ namespace CityInfo.API
         {
             Cities = new List<CityDto>()
             {
-                new CityDto() { Id = 1, Name = "New York City", Description = "The one is the most populous city in the United States."},
-                new CityDto() { Id = 2, Name = "Ho Chi Minh City", Description= "The one also known by its former name Saigon, is the largest city in Vietnam. "}
+                
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one is the most populous city in the United States.",
+                   PointsOfInterest = new List<PointOfInterestDto>()
+                   {
+                       new PointOfInterestDto()
+                       {
+                           Id=1,    
+                           Name="Central Park",
+                           Description="Peaceful vibe."
+                       },
+                       new PointOfInterestDto()
+                       {
+                           Id=2,
+                           Name="Statue of Liberty",
+                           Description="The most iconic sight of the US."
+                       }
+                   }
+                },
+                new CityDto()
+                {
+                    Id=2,
+                    Name = "Ho Chi Minh City",
+                    Description="One of the most popular city of Vietnam.",
+                    PointsOfInterest= new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id=1,
+                            Name="Motobike Riding",
+                            Description="Exciting experience for foreigners."
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id=2,
+                            Name="Strett Foods",
+                            Description="Various delicious foods with the appropreate price."
+                        }
+                    }
+                }
             };
         }
+
     }
 }
