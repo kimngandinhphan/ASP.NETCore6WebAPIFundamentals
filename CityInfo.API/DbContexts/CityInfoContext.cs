a623292224fd487fcf04c03224aa6663974b0fe3
Kim Ngan Dinh Phan <kimngandinhphan@gmail.com>
2022-08-15 22:50:34 +0700

    add entities

---
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -0,0 +1,11 @@
+﻿using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.DbContexts
+{
+    public class CityInfoContext : DbContext
+    {
+        public DbSet<City> Cities { get; set; } = null!;
+        public DbSet<PointOfInterest> PointsOfInterests { get; set;} = null!;
+    }
+}
